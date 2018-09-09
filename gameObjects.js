f2b018a1f65114c207de5d82dec3933d4dc5ea55
sahiljohari <darewithsj@gmail.com>
2018-09-09 15:52:45 -0500

    Basic implementation

--- gameObjects.js
+++ gameObjects.js
@@ -25,3 +25,21 @@ class Base{
         this.canvas.rect(this.x, this.y, this.width, this.height);
     }
 }
+
+class Tank{
+    constructor(canvas, x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.canvas = canvas;
+    }
+
+    display(){
+        this.canvas.rect(this.x, this.y, this.width, this.height);
+    }
+
+    collide(obstacle) {
+
+    }
+}
