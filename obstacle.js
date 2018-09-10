4df1fb8f46be0f803f8c86cf951b80e54ee89cf5
sahiljohari <darewithsj@gmail.com>
2018-09-09 22:17:10 -0500

    Base code ready

---
+++ obstacle.js
@@ -0,0 +1,13 @@
+class Mountain{
+    constructor(canvas, x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.canvas = canvas;
+    }
+
+    display(){
+        this.canvas.rect(this.x, this.y, this.width, this.height);
+    }
+}
