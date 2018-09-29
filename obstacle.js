1aa54db97bb017535dd0014080def51c5a8f59ba
sahiljohari <darewithsj@gmail.com>
2018-09-28 22:57:28 -0500

    perfected collision

--- obstacle.js
+++ obstacle.js
@@ -1,13 +1,18 @@
-class Mountain{
-    constructor(canvas, x, y, width, height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.canvas = canvas;
-    }
+class Mountain {
+  constructor(canvas, x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.centerX = (this.x + this.width) / 2;
+    this.centerY = (this.y + this.height) / 2;
+    this.halfWidth = this.width / 2;
+    this.halfHeight = this.height / 2;
 
-    display(){
-        this.canvas.rect(this.x, this.y, this.width, this.height);
-    }
+    this.canvas = canvas;
+  }
+
+  display() {
+    this.canvas.rect(this.x, this.y, this.width, this.height);
+  }
 }
